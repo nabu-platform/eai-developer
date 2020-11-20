d97aae3481236ec367763fc4569b0078079a7dcd
Alexander Verbruggen <alex@nabu.be>
2020-11-20 17:16:21 +0100

    added initial application icon

--- src/main/java/be/nabu/eai/developer/collection/ProjectManagerFactory.java
+++ src/main/java/be/nabu/eai/developer/collection/ProjectManagerFactory.java
@@ -3,11 +3,17 @@ package be.nabu.eai.developer.collection;
 import java.util.ArrayList;
 import java.util.List;
 
+import be.nabu.eai.developer.CollectionActionImpl;
+import be.nabu.eai.developer.MainController;
 import be.nabu.eai.developer.api.CollectionAction;
 import be.nabu.eai.developer.api.CollectionManager;
 import be.nabu.eai.developer.api.CollectionManagerFactory;
 import be.nabu.eai.repository.api.Collection;
 import be.nabu.eai.repository.api.Entry;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class ProjectManagerFactory implements CollectionManagerFactory {
 
@@ -29,7 +35,18 @@ public class ProjectManagerFactory implements CollectionManagerFactory {
 		Collection collection = entry.getCollection();
 		// for projects, you can add applications
 		if (collection != null && collection.getType().equals("project")) {
-			
+			VBox box = new VBox();
+			box.getStyleClass().add("collection-action");
+			Label title = new Label("Add Application");
+			title.getStyleClass().add("collection-action-title");
+			box.getChildren().addAll(MainController.loadFixedSizeGraphic("application/application-big.png", 64), title);
+			actions.add(new CollectionActionImpl(box, new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent arg0) {
+					// TODO
+					// for an application we only need a name?
+				}
+			}));
 		}
 		return actions;
 	}
